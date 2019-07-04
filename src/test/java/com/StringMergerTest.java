93c427e0d9a6aa3f9e871e0af78c8a9c319474f6
Austin Lautissier <k1461615@users.noreply.github.com>
2019-07-04 16:52:36 +0100

    initial test upload

---
+++ src/test/java/com/StringMergerTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringMergerTest {
+
+	@org.junit.jupiter.api.Test
+	void mergeString() {
+		StringMerger merger = new StringMerger.StringMergerImpl();
+
+		// intersect is at the end of a and the start of b with 1 char of overlap
+		assertEquals("abcde", merger.mergeStrings("abc", "cde"));
+		// intersect is at the end of a and the start of b with more than 1 char of overlap
+		assertEquals("abcde", merger.mergeStrings("abcd", "cde"));
+		// b is a proper subset of a
+		assertEquals("abcde", merger.mergeStrings("abcde", "bcd"));
+		// intersect is at the end of a and a contains duplication
+		assertEquals("abcdcde", merger.mergeStrings("abcdcd", "cde"));
+		// intersect is at the end of a and a contains duplication
+		assertEquals("abcde", merger.mergeStrings("cde", "abc"));
+	}
+}
